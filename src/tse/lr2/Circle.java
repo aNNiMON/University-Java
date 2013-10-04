857973a51e284e0396922929fa97e0f7fbfe72a3
Victor <melnikukr@mail.ru>
2013-10-04 17:18:11 +0300

    Добавлена функция проверки принадлежности точки к окружности

--- src/tse/lr2/Circle.java
+++ src/tse/lr2/Circle.java
@@ -1,13 +1,21 @@
 package tse.lr2;
 
+import java.awt.Point;
+
 /**
  *
  * @author aNNiMON
  */
 public class Circle extends Ellipse {
 
-    public boolean isConsist() {
-        return false;
+    public boolean isConsist(Point point) {
+        Point center = getCenterPoint();
+        int radius = center.x - points[0].x;
+        
+        double tmp = Math.pow(point.x - center.x, 2);
+        tmp += Math.pow(point.y - center.y, 2);
+        
+        return (tmp <= (radius * radius));
     }
     
     @Override
